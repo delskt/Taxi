c1ebd251dce2346d4ff8e7a9537917e8245c1478
Daniel Silva dos Passos <delskt@outlook.com>
2020-03-01 19:43:45 -0300

    Add Projects web, xamarin and common. Add Entity Taxi. Configure DataContext. Add InitialDb.

---
+++ Taxi.Web/Data/Entities/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TaxiEntity> Taxis { get; set; }
+    }
+}
